602476f7dc134954142c6cd354856eade7d21b99
zeid <zeidhassan4@gmail.com>
2024-02-03 22:38:47 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,14 +3,12 @@ import React from "react";
 
 function EyesOnMe () {
     function clickFn(focus, blur) {
-       if (focus) {
         console.log('Good');
-       }else{
-        console.log('Hey! Eyes on me!');
-       }
+        console.log('Hey!Eyes on me!');
+       
     }
     return (
-       <button onClick={ clickFn([0], [1]) }>'Eyes on me'</button>
+       <button onClick={ clickFn }>'Eyes on me'</button>
     )
 }
 export default EyesOnMe
